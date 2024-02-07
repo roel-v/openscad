704cba00ecd4dca3ffc50505d8f6c089a91a6e01
Roel Vanhout <roel.vanhout@gmail.com>
2024-02-07 15:47:42 +0100

    Make msvc build script work without absolute paths, so user no longer has to edit paths manually.

--- build_vcpkg.bat
+++ build_vcpkg.bat
@@ -1,13 +1,35 @@
-vcpkg install
+@echo off
+setlocal enabledelayedexpansion
+
+rem Find vcpkg root first so that we can bail early if it's not found.
+if not defined VCPKG_ROOT (
+    echo VCPKG_ROOT not set, attempting to locate vcpkg.exe...
+
+    for /f "tokens=*" %%i in ('where vcpkg.exe 2^>nul') do (
+        set VCPKG_EXE_PATH=%%i
+        goto found
+    )
+
+    echo vcpkg.exe not found in PATH.
+    goto end
+
+    :found
+    for %%i in ("%VCPKG_EXE_PATH%") do (
+        set VCPKG_ROOT=%%~dpi
+    )
+    set VCPKG_ROOT=%VCPKG_ROOT:~0,-1%
+
+    echo Set VCPKG_ROOT to %VCPKG_ROOT%
+)
+
+vcpkg install --triplet x64-windows
 
 mkdir build
 
-rem --trace ^
 cmake -B build -S . ^
-    -DCMAKE_TOOLCHAIN_FILE=d:/vcpkg/scripts/buildsystems/vcpkg.cmake ^
+    -DCMAKE_TOOLCHAIN_FILE=%VCPKG_ROOT%/scripts/buildsystems/vcpkg.cmake ^
     -DUSE_BUILTIN_OPENCSG=TRUE ^
     -DENABLE_CAIRO=FALSE ^
-    -DCMAKE_PREFIX_PATH=e:\Dropbox\bin\win_flex_bison-2.5.25 ^
     -DCMAKE_EXE_LINKER_FLAGS="/manifest:no" ^
     -DCMAKE_MODULE_LINKER_FLAGS="/manifest:no" ^
     -DCMAKE_SHARED_LINKER_FLAGS="/manifest:no"
